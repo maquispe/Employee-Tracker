00d0fd758886d4b218d12d8165151231ccfbc8f2
maquispe <62685159+maquispe@users.noreply.github.com>
2020-07-02 00:07:23 -0700

    adds initial sql database

---
+++ employee.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department(name) VALUE ("Fraud");
+INSERT INTO department(name) VALUE ("Customer Support");
+INSERT INTO department(name) VALUE ("Engineering");
+
+INSERT INTO role(title, salary) VALUES ("Manager", "80000");
+INSERT INTO role(title, salary) VALUES ("Software Engineer", "100000");
+INSERT INTO role(title, salary) VALUES ("Fraud Specialist", "90000");
+
+INSERT INTO employee(first_name, last_name) VALUES ("Canciano", "Elizarraras");
+INSERT INTO employee(first_name, last_name) VALUES ("Jasmine", "Parker");
+INSERT INTO employee(first_name, last_name) VALUES ("Cindy", "Alvarado");
