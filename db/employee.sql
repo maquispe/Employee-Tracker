837213377d80a5180d45e47fd610eb79cf107976
maquispe <62685159+maquispe@users.noreply.github.com>
2020-07-31 00:46:18 -0700

    Fixes schema

--- db/employee.sql
+++ db/employee.sql
@@ -2,23 +2,28 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
+    INDEX department_id_idx (department_id),
+    CONSTRAINT department_id FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    INDEX role_id_idx (role_id),
+    INDEX manager_id_idx (manager_id),
+    CONSTRAINT role_id FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT manager_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-
